72cdfe0c939d04d914bf7fdc4ea277533e24d4b4
Yogesh <yogeshoneness730@gmail.com>
2026-01-11 23:49:41 +0530

    content updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Stats />
+      {/* <Stats /> */}
       <LiveProjects />
       <ProjectGallery />
       <WhyChooseUs />
